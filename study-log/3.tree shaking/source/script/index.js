5eb2487591ce9a54b8745224453d25fb0e633346
Charles <woyiweita@sina.com>
2018-06-15 18:32:39 +0800

    tree shaking 功能扩展

---
+++ study-log/3.tree shaking/source/script/index.js
@@ -0,0 +1,15 @@
+import {cube} from './math.js';
+import '../style/css.css';
+
+function component(){
+    let element = document.createElement('pre');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cube is equal to ' + cube()
+    ].join('\n\n');
+    return element;
+}
+
+const body = document.body;
+let element = component();
+body.appendChild(element);
